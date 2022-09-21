a363fd174bab678130b15785272bf2fccb04e341
mariaSerrabona <83339874+mariaSerrabona@users.noreply.github.com>
2022-09-21 09:07:02 +0200

    validación de dni

--- gestor/menu.py
+++ gestor/menu.py
@@ -29,16 +29,19 @@ def iniciar():
             print(cliente) if cliente else print("Cliente no encontrado.")
         if opcion == '3':
             print("Añadiendo un cliente...\n")
-            dni = helpers.leer_texto(
-                3, 3, "DNI (2 ints y 1 char)").upper()
-            nombre = helpers.leer_texto(
-            2, 30, "Nombre (de 2 a 30 chars)").capitalize()
+            # Comprobación de DNI válido
+            while 1:
+                dni = helpers.leer_texto(3, 3, "DNI (2 ints y 1 char)").upper()
+                if helpers.dni_valido(dni, db.Clientes.lista):
+                    break
+            nombre = helpers.leer_texto(2, 30, "Nombre (de 2 a 30 chars)").capitalize()
             apellido = helpers.leer_texto(2, 30, "Apellido (de 2 a 30 chars)").capitalize()
             db.Clientes.crear(dni, nombre, apellido)
-            print("Cliente añadido correctamente.")
+
         if opcion == '4':
             print("Modificando un cliente...\n")
-            dni = helpers.leer_texto(3, 3, "DNI (2 ints y 1 char)").upper() cliente = db.Clientes.buscar(dni)
+            dni = helpers.leer_texto(3, 3, "DNI (2 ints y 1 char)").upper()
+            cliente = db.Clientes.buscar(dni)
             if cliente:
                 nombre = helpers.leer_texto(2, 30, f"Nombre (de 2 a 30 chars)[{cliente.nombre}]").capitalize()
                 apellido = helpers.leer_texto(2, 30, f"Apellido (de 2 a 30 chars) [{cliente.apellido}]").capitalize()
