946bc7340aa778de1f3e8fbe2b13280d03578b62
mariaSerrabona <83339874+mariaSerrabona@users.noreply.github.com>
2022-09-21 09:26:05 +0200

    empezando la parte gráfica

---
+++ gestor/ui.py
@@ -0,0 +1,17 @@
+from tkinter import *
+
+class MainWindow(Tk):
+    def __init__(self):
+        super().__init__()
+        self.title('Gestor de clientes')
+        self.build()
+    def build(self):
+        button = Button(self.root, text="Hola", command=self.hola)
+        button.pack()
+    def hola(self):
+        print("¡Hola mundo!")
+
+
+if __name__ == "__main__":
+    app = MainWindow()
+    app.mainloop()
